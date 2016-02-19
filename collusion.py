ea2060d0707588042643e62d63d50d47b80759b6
Jorge Blasco <guizos@gmail.com>
2016-02-19 18:45:23 +0000

    removes external storage rules as they are now included in the code as an option

--- collusion.py
+++ collusion.py
@@ -1,3 +1,5 @@
+from acid_detectors.utils import get_all_in_dir
+
 __author__ = 'jorgeblasco'
 import re
 import os.path
@@ -9,7 +11,8 @@ from subprocess import check_output
 
 class CollusionSet:
 
-    def __init__(self,app_list,channels):
+    def __init__(self,kind,app_list,channels):
+        self.kind = kind
         self.app_list = app_list
         self.channels = channels
 
@@ -17,7 +20,7 @@ class CollusionSet:
         if len(self.app_list)==0:
             return ""
         else:
-            result = self.app_list[0]
+            result = self.kind+"\n"+self.app_list[0]
             for i in range(0,len(self.channels),1):
                 result = result +"-->" + self.channels[i]+ "-->"+ self.app_list[i+1]
             return result
@@ -29,12 +32,14 @@ class CollusionSet:
         print "+++++++++++++++"
         print " Colluding Set"
         print "+++++++++++++++"
+        print self.kind
+        print "--------------"
         print self.app_list
         print "-- Channels --"
         print self.channels
 
 
-colluding_predicates = ['colluding','colluding_info', 'colluding_money1','colluding_money2','colluding_service']
+colluding_predicates = ['colluding_info', 'colluding_money1','colluding_money2','colluding_service']
 
 def find_all_colluding(colluding_predicate,rule_file):
     if colluding_predicate not in colluding_predicates:
@@ -62,6 +67,24 @@ def communication_channel(app_set,rule_file):
     channel_list = result.replace(",","\',\'")
     channel_list = "[\'"+channel_list[1:-2]+"\']"
     return ast.literal_eval(channel_list)
+
+
+def filter_intents(rule_file,intent_filters_folder):
+    filtered_file = "filtered_file"
+    files = get_all_in_dir(intent_filters_folder,"*")
+    lines_to_remove = []
+    for file in files:
+        with open(file,'r') as r:
+            lines_to_remove.extend([line[:-1] for line in r.readlines()])
+    with open(filtered_file,'w') as f:
+        with open(rule_file,'r') as rulef:
+            rule_lines = rulef.readlines()
+            final_lines = [rule for rule in rule_lines if len([line for line in lines_to_remove if line in rule])==0]
+            f.writelines(final_lines)
+    return filtered_file
+
+
+
 
 
 
